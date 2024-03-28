e467f147d3ddd85278a7d80cb6ea8e9d64581443
funkstermonster <fanni.szasz96@gmail.com>
2024-03-28 12:30:46 +0100

    change description input tag into textarea, add control for it

--- app/admin/product/page.tsx
+++ app/admin/product/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Button, Input } from "@nextui-org/react";
+import { Button, Input, Textarea } from "@nextui-org/react";
 import { CldUploadWidget } from "next-cloudinary";
 import { toast } from "sonner";
 import { productFormSchema } from "@/schemas/schema";
 import { Productdata } from "@/types/product";
 import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
 
 export default function ProductForm() {
+  const [value, setValue] = React.useState("");
   const [imageUrl, setImageUrl] = useState<string>("");
 
   const {
@@ -18,12 +20,12 @@ export default function ProductForm() {
   } = useForm<Productdata>({ resolver: zodResolver(productFormSchema) });
 
   const handleUploadSuccess = (response: any) => {
-    console.log(response.url)
+    console.log(response.url);
     setImageUrl(response.url);
   };
 
   const onSubmit = async (formData: Productdata) => {
-    console.log("submitted")
+    console.log("submitted");
     const { name, price, description } = formData;
 
     try {
@@ -52,7 +54,17 @@ export default function ProductForm() {
         <div className="flex flex-col gap-2 mx-auto max-w-md mt-10">
           <Input type="text" label="Product name" {...register("name")} />
           <Input type="number" label="Price" {...register("price")} />
-          <Input type="text" label="Description" {...register("description")} />
+          <Textarea
+            value={value}
+            onValueChange={setValue}
+            isInvalid={value.length > 10}
+            variant="bordered"
+            placeholder="Enter your description"
+            type="text"
+            {...register("description")}
+            label="Description"
+            errorMessage= {value.length > 10 ? "The description should be maximum 10 characters long." : ""} 
+          />
           <h1>Upload an image</h1>
           <CldUploadWidget
             uploadPreset="qwut2dzj"
