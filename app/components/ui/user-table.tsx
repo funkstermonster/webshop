851b195dc48d6da6e5a327238076c7bba9d7bbb7
funkstermonster <fanni.szasz96@gmail.com>
2024-04-05 13:34:31 +0200

    add infinite scroll package to try it, and created pagination isntead of infinite scroll

--- app/components/ui/user-table.tsx
+++ app/components/ui/user-table.tsx
@@ -1,13 +1,23 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { User } from "@/types/user";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Pagination,
+} from "@nextui-org/react";
+import React from "react";
 
 interface RegisteredUsersProps {
-    registeredUsers: User[];
+  registeredUsers: User[];
 }
-  
+
 const columns = [
   {
     key: "name",
@@ -22,64 +32,93 @@ const columns = [
     label: "ROLE",
   },
   {
-      key: "registeredAt",
-      label: "REGISTERED AT"
-  }
+    key: "registeredAt",
+    label: "REGISTERED AT",
+  },
 ];
 
 export function TableForUser({ registeredUsers }: RegisteredUsersProps) {
-    const [users, setUsers] = useState<User[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        const fetchUsers =  () => {
-            fetch(`/api/user`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Failed to fetch users');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    setUsers(data);
-                })
-                .catch(error => {
-                    console.error('Error fetching users:', error);
-                    setError('Failed to fetch users');
-                })
-                .finally(() => {
-                    setLoading(false);
-                });
-        };
+  const [page, setPage] = React.useState(1);
+  const rowsPerPage = 5;
 
-        fetchUsers();
-    }, []);
+  const pages = Math.ceil(users.length / rowsPerPage);
 
-    if (loading) {
-        return <p>Loading...</p>;
-    }
+  const items = React.useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
 
-    if (error) {
-        return <p>{error}</p>;
-    }
+    return users.slice(start, end);
+  }, [page, users]);
 
-    return (
-        <Table aria-label="Example table with dynamic content" className="dark">
-            <TableHeader columns={columns}>
-                {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-            </TableHeader>
-            <TableBody items={users}>
-                {(item) => (
-                    <TableRow key={item.id}>
-                        {columns.map(column => (
-                            <TableCell key={column.key}>
-                                {getKeyValue(item, column.key)}
-                            </TableCell>
-                        ))}
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
-    );
+  useEffect(() => {
+    const fetchUsers = () => {
+      fetch(`/api/user`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to fetch users");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setUsers(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching users:", error);
+          setError("Failed to fetch users");
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <Table
+      aria-label="Example table with dynamic content"
+      className="dark"
+      bottomContent={
+        <div className="flex w-full justify-center">
+          <Pagination
+            isCompact
+            showControls
+            showShadow
+            color="secondary"
+            page={page}
+            total={pages}
+            onChange={(page) => setPage(page)}
+          />
+        </div>
+      }
+      classNames={{ wrapper: "min-h-[222px" }}
+    >
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={items}>
+        {(item) => (
+          <TableRow key={item.id}>
+            {columns.map((column) => (
+              <TableCell key={column.key}>
+                {getKeyValue(item, column.key)}
+              </TableCell>
+            ))}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
 }
